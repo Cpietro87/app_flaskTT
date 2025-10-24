8aa6d251beb629ba5d72efda7c6f79322b2ef3c0
Cpietro87 <cristianpietrobon2019@gmail.com>
2025-10-23 21:10:03 -0300

    se agregó CRUD users

--- routes/routes_users.py
+++ routes/routes_users.py
@@ -4,11 +4,15 @@ from models.users import User
 
 routes_users = Blueprint('routes_users', __name__, url_prefix='/users')
 
+#Funcion CRUD users
+
+#Read
 @routes_users.route('/')
 def get_users():
     users = User.query.all()
     return render_template('users/users.html', users=users)
 
+#Delete
 @routes_users.route('/delete/<int:id>')
 def delete_user(id):
     user = User.query.get(id)
@@ -17,6 +21,7 @@ def delete_user(id):
         db.session.commit()
     return redirect(url_for('routes_users.get_users'))
 
+#Update
 @routes_users.route('/update/<int:id>', methods=['GET'])
 def edit_user(id):
     user = User.query.get(id)
@@ -24,9 +29,31 @@ def edit_user(id):
 
 @routes_users.route('/update/<int:id>', methods=['POST'])
 def update_user(id):
-    return f"User {id} updated"
+    user = User.query.get(id)
+    user.username = request.form['username']
+    user.email = request.form['email']
+    user.password = request.form['password']
+    db.session.commit()
 
-@routes_users.route('/register')
+    return redirect(url_for('routes_users.get_users'))
+
+
+#Create
+@routes_users.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        username =request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+
+        existing_user = User.query.filter((User.email == email)).first()
+        if existing_user:
+            return render_template('users/register.html', error="User already exists") 
+        
+        new_user = User(username=username, email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('routes_users.get_users'))
+
     return render_template('users/register.html')
 
